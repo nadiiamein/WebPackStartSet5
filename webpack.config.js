60b11ddb7e74ee59e5fba3b7bf5ed33bcaafe573
Nadiia <npodufala15@gmail.com>
2020-12-20 14:36:58 +0200

    console.log

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,10 @@ module.exports = {
               test: /\.css$/i,
               use: [MiniCssExtractPlugin.loader, 'css-loader'],
           },
+          {
+            test: /\.s[ac]ss$/i,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
       ],
   },
 };
