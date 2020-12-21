b779b2efe006e87dd47d72a905ec37fa3f8cd6ed
Nadiia <npodufala15@gmail.com>
2020-12-21 20:55:58 +0200

    console.log

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin =require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -37,10 +38,20 @@ module.exports = {
       new MiniCssExtractPlugin({
         filename: `./css/${filename('css')}`,
 
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/assets'), to: path.resolve(__dirname, 'app') }
+        ]
       })
   ],
   module:{
       rules:[
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
           {
               test: /\.css$/i,
               use:
